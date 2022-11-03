b8f9793c5388df51464e65a86d626bae5e874073
Vicky Brooke <vicky.brooke@sky.uk>
2022-11-03 14:54:34 +0000

    Associations added to index.js

--- models/index.js
+++ models/index.js
@@ -2,4 +2,12 @@ const Board = require("./board.model");
 const Cheese = require("./cheese.model");
 const User = require("./user.model");
 
+// One-to-many relationship
+User.hasMany(Board);
+Board.belongsTo(User);
+
+//Many-to-many relationship
+Board.belongsToMany(Cheese, { through: Board_Cheese });
+Cheese.belongsToMany(Board, { through: Board_Cheese });
+
 module.exports = { Board, Cheese, User };
